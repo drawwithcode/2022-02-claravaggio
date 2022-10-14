00a9b13bdf2b3e4be327d1d2eabef09cd063a692
claravaggio <clara.dibella@mail.polimi.it>
2022-10-14 18:39:17 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,33 @@ function preload() {
 	// put preload code here
 }
 
+let yoff= 0.0;
+let moving = 0;
+
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
 }
 
 function draw() {
-	// put drawing code here
+	background(51);
+	moving += 1;
+
+	beginShape();
+	fill(255, 0, 0, 50);
+	
+  
+	let xoff = 0; 
+  
+	// Iterate over horizontal pixels
+	for (let x = 0; x <= width; x += 5) {
+	  let y = map(noise(xoff, yoff), 0, 0.2, 20*!moving, moving);
+	  vertex(x, y);
+	  xoff += 0.02;
+	}
+	yoff += 0.01;
+	vertex(width, height);
+	vertex(0, height);
+	endShape();
 }
+
